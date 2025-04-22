8a434ec8b05d2498aa22608e411908fcfa724a36
LuuTruongVan <luutruongvan1607@gmail.com>
2025-04-22 22:21:32 +0700

    EmployeeForm, LoginForm, ForgetPassForm

---
+++ WinFormsApp1/WinFormsApp1/LoginForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WinFormsApp1
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.875F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(385, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 71);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(29, 179);
+            label2.Name = "label2";
+            label2.Size = new Size(218, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Username/Email:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(56, 302);
+            label3.Name = "label3";
+            label3.Size = new Size(134, 37);
+            label3.TabIndex = 2;
+            label3.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(263, 179);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(617, 43);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(263, 302);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(617, 43);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(741, 447);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 71);
+            button1.TabIndex = 5;
+            button1.Text = "Sign In";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(558, 447);
+            button2.Name = "button2";
+            button2.Size = new Size(155, 67);
+            button2.TabIndex = 6;
+            button2.Text = "Sign Up";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(56, 447);
+            button3.Name = "button3";
+            button3.Size = new Size(243, 67);
+            button3.TabIndex = 7;
+            button3.Text = "Forget Password";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(976, 563);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
--- WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b27f2c068048d0b7bbdaa49ee28688d361154c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
