1a7bec4e6fde89d07f403c925609046d3f1aa48d
tthdong79 <2251120008@ut.edu.vn>
2025-05-03 21:23:03 +0700

    Thêm IntroduceForm và logo

--- WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d06e7d1b76c593810e49d8ae7905471ffe0243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b54de7e790cea34363649dd8d86f2b5540b1851")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
